9af5669828cbda796a3bf9ea8fe9b33568d4aac0
AbduraimovErlan <96403251+AbduraimovErlan@users.noreply.github.com>
2024-10-18 13:56:59 -0500

    adding the assert

--- test_simple_example.py
+++ test_simple_example.py
@@ -5,6 +5,8 @@ import pytest
 @pytest.mark.sanity
 def test_my_first_one():
     print("\n i'm inside my first test!")
+    my_validation = True
+    assert my_validation, "The validation of this test has failed!"
 
 
 def test_my_second_one():
